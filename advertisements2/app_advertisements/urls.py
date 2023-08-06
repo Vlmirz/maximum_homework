26e320b8f3c0ba05f0b9fd64a0d07d41e011effd
Vlmirz <136348300+Vlmirz@users.noreply.github.com>
2023-08-06 19:26:06 +0400

    Delete advertisements2 directory

--- advertisements2/app_advertisements/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import index, top_sellers
-
-
-urlpatterns = [
-    path('', index, name='main'),
-    path('top_sellers', top_sellers, name='top_sellers')
-]
